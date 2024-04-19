27c9c70a0fc133ab9f5f5a9dd277e45eaba12d90
john <djnole0129@gmail.com>
2024-04-19 02:22:07 +0000

    add comments in preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -1,8 +1,9 @@
+# script to decode tfrecord file
 python preprocess.py \
     --data_root /opt/data/private/emernerf/data/waymo/raw \
     --target_dir data/waymo/processed \
     --split training \
     --process_keys images lidar calib pose dynamic_masks \
-    --workers 4 \
-    --scene_ids 1 \
+    --workers 8 \
+    --scene_ids 546 581 592 620 640 700 754 795 796
     # --split_file data/waymo_splits/dynamic32.txt

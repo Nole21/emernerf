0381e4cbe96ef1bc544b2770cb576ff3624a9860
john <djnole0129@gmail.com>
2024-04-22 08:22:39 +0000

    revise compute_metrics.sh

--- compute_metrics.sh
+++ compute_metrics.sh
@@ -1,20 +1,40 @@
 # $1 is the experiment dir
-psnr=0
-ssim=0
-masked_psnr=0
-masked_ssim=0
-count=0
-for scene in $(ls $1);do
-    count=$count+1
-    for log in /opt/data/private/emernerf/experiments/$1/$scene/logs;do
-        awk  -v psnr=$psnr ssim=$ssim masked_psnr=$masked_psnr masked_ssim=$masked_ssim'
-        {BEGIN FS=" "}
-        /PSNR:/ && NR>=600 {psnr+=$NF}  
-        /SSIM:/ && NR>=600 {ssim+=$NF)}
-        /Masked PSNR:/ && NR>=600 {masked_psnr+=$NF}
-        /Masked SSIM:/ && NR>=600 {masked_ssim+=$NF}
-        ' /opt/data/private/emernerf/experiments/$1/$scene/logs/$log
-    done
+for metric in PSNR SSIM Masked_PSNR Masked_SSIM; do
+    for scene in $(ls /opt/data/private/emernerf/experiments/$1); do
+        for file in $(ls /opt/data/private/emernerf/experiments/$1/$scene/logs); do
+            if [ $metric == PSNR ]
+            then
+                awk 'BEGIN {FS=" "}
+                NR==600 {print "'$scene'",": ", $NF}
+                ' /opt/data/private/emernerf/experiments/$1/$scene/logs/$file
+            elif [ $metric == SSIM ]
+            then
+                awk 'BEGIN {FS=" "}
+                NR==601 {print "'$scene'",": ", $NF}
+                ' /opt/data/private/emernerf/experiments/$1/$scene/logs/$file
+            elif [ $metric == Masked_PSNR ]
+            then
+                awk 'BEGIN {FS=" "}
+                NR==603 {print "'$scene'",": ", $NF}
+                ' /opt/data/private/emernerf/experiments/$1/$scene/logs/$file
+            else 
+                awk 'BEGIN {FS=" "}
+                NR==604 {print "'$scene'",": ", $NF}
+                ' /opt/data/private/emernerf/experiments/$1/$scene/logs/$file
+            fi
+        done
+    done > /opt/data/private/emernerf/$metric.txt
 done
+
+for metric in PSNR SSIM Masked_PSNR Masked_SSIM; do
+    awk 'BEGIN {FS=" ";sum=0}
+    {sum+=$NF}
+    END {sum/=NR; print "'$metric'", ": ", sum}
+    ' /opt/data/private/emernerf/$metric.txt
+    rm /opt/data/private/emernerf/$metric.txt
+done
+
+
+
 
 
--- train_emernerf.sh
+++ train_emernerf.sh
@@ -1,20 +1,22 @@
-scene_idx=1
+# 31 34 35 49 53 80 84 86 89 94 96 102 111 222 323 382 402 427 438 546 581 592 620 640 700 754 795 796 
 start_timestep=0
 end_timestep=-1
 project=nvs_affine
 num_iters=25000
-python train_emernerf.py \
-    --config_file configs/default_flow.yaml \
-    --output_root /opt/data/private/emernerf/experiments \
-    --project $project \
-    --run_name ${scene_idx}_flow \
-    data.scene_idx=$scene_idx \
-    data.start_timestep=$start_timestep \
-    data.end_timestep=$end_timestep \
-    data.pixel_source.load_features=True \
-    data.pixel_source.feature_model_type=dinov2_vitb14 \
-    nerf.model.head.enable_feature_head=True \
-    nerf.model.head.enable_learnable_pe=True \
-    logging.saveckpt_freq=$num_iters \
-    optim.num_iters=$num_iters \
-    data.pixel_source.test_image_stride=10 
+for scene_idx in 16 21 22 25 31 34 35 49 53 80 84 86 89 94 96 102 111 222 323 382 402 427 438 546 581 592 620 640 700 754 795 796;do
+    python train_emernerf.py \
+        --config_file configs/default_flow.yaml \
+        --output_root /opt/data/private/emernerf/experiments \
+        --project $project \
+        --run_name ${scene_idx}_flow \
+        data.scene_idx=$scene_idx \
+        data.start_timestep=$start_timestep \
+        data.end_timestep=$end_timestep \
+        data.pixel_source.load_features=True \
+        data.pixel_source.feature_model_type=dinov2_vitb14 \
+        nerf.model.head.enable_feature_head=True \
+        nerf.model.head.enable_learnable_pe=True \
+        logging.saveckpt_freq=$num_iters \
+        optim.num_iters=$num_iters \
+        data.pixel_source.test_image_stride=10 
+done
